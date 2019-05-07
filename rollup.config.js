d474884d854c88b0b0c68c3da523c5214321ec25
Kiho <kihoc@gmail.com>
2019-05-06 23:24:44 -0700

    Fix production build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import buble from 'rollup-plugin-buble';
-import uglify from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -19,28 +18,23 @@ export default {
 			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
-			// a separate file  better for performance
-			// css: css => {
-			// 	css.write('public/app.css');
-			// },
-			// onwarn: (warning, handler) => {
-			// 	if (warning.code === 'a11y-missing-href') return;
-			// 	handler(warning);
-			// }
+			// a separate file — better for performance
+			css: css => {
+				css.write('public/bundle.css');
+			}
 		}),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration 
+		// some cases you'll need additional configuration —
 		// consult the documentation for details:
 		// https://github.com/rollup/rollup-plugin-commonjs
 		resolve(),
 		commonjs(),
 
 		// If we're building for production (npm run build
-		// instead of npm run dev), transpile and minify
-		production && buble({ exclude: 'node_modules/**' }),
-		production && uglify()
+		// instead of npm run dev), minify
+		production && terser()
 	]
 };
 
